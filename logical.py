cb8c379d7949009f7b304613126d4aef1dfa28fa
monikaprajapt <92983459+monikaprajapt@users.noreply.github.com>
2021-11-25 19:06:03 +0530

    if else

---
+++ logical.py
@@ -0,0 +1,56 @@
+num=int(input("enter the number"))
+# if num=="-":
+#     print(num)
+# else:
+#     print(-num)
+
+
+
+# list=[1,2,3,4,5,6,7,8,9,11,12,5,4,4,6]
+# b=[]
+# size=5
+# for i in range(0,len(list),size):
+#     b.append(list[i:i+size])
+# print(b)
+
+
+
+# amount=1345
+# b=amount//1000
+# c=amount//200
+# d=amount//100
+# e=amount//10
+# f=amount//5
+# if amount//1000:
+#     print(b,"note 1000")
+# if amount//200:
+#     print(c,"note 200")
+# if amount//100:
+#     print(d,"note 100")
+# if amount//10:
+#     print(e,"note 10")
+# if amount//5:
+#     print(f,"note 5")
+# else:
+#     print("error")
+
+
+# amount=int(input("enter the number"))
+# if amount>=2000:
+#     note2000=amount//2000
+#     amount=amount-note2000*2000
+#     print("2000 \t=\t",note2000)
+
+# if amount>=500:
+#     note500=amount//500
+#     amount=amount-note500*500
+#     print("500 \t=\t",note500)
+# else:
+#     print("ok")
+
+num=int(input("enter the number"))
+if num>=1000 and num<=9999:
+    print("4 se dight number")
+else:
+    print("not 4 se digit")
+
